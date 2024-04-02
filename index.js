42b386ed2f71fd11d499e42dd05da872da8af236
CKsisodia <cksisodia3@gmail.com>
2024-04-03 01:06:39 +0530

    routing

--- index.js
+++ index.js
@@ -8,6 +8,24 @@ const handler = (req, res) => {
       "<html><head><title>My First Page</title></head><body><h1>Hello World</h1></body></html>"
     );
     res.end();
+  } else if (url === "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      "<html><head><title>My First Page</title></head><body><h1>Welcome Home</h1></body></html>"
+    );
+    res.end();
+  } else if (url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      "<html><head><title>My First Page</title></head><body><h1>About us Page</h1></body></html>"
+    );
+    res.end();
+  } else if (url === "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      "<html><head><title>My First Page</title></head><body><h1>Welcome to myy NOde js project</h1></body></html>"
+    );
+    res.end();
   }
 };
 const server = http.createServer(handler);
