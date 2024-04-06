eb837a3b3fc95d283f9790504716eec227502c7b
CKsisodia <cksisodia3@gmail.com>
2024-04-06 19:38:46 +0530

    routes

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
-app.use((res, req, next) => {
-    console.log("IN the middleware")
-    next()
-})
-app.use((req, res, next) => {
-    console.log("IN the middleware")
-   res.send({ key1: "HelloSharpener" })
-})
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/", (req, res, next) => {
+  next();
+});
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST" ><input type="text" name="Title"/><input type="text" name="Description"/><button type="submit">Add Product</button></form>'
+  );
+});
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello Sharpener</h1>");
+});
 
 app.listen(3000);
