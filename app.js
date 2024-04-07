a6d143f9ecdba3e162ccf475093ad47f146d01bb
CKsisodia <cksisodia3@gmail.com>
2024-04-07 17:59:58 +0530

    404 page,admin route and cleanupcode

--- app.js
+++ app.js
@@ -3,22 +3,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", (req, res, next) => {
   next();
 });
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST" ><input type="text" name="Title"/><input type="text" name="Description"/><button type="submit">Add Product</button></form>'
-  );
-});
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello Sharpener</h1>");
-});
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not Found 🧑‍💻</h1>')
+})
+
 
 app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get("/", (req, res, next) => {
+    res.send("<h1>Hello Sharpener</h1>");
+  });
+
+
+
+module.exports = router
