b700fd840363f7fc054da16d9d649fd45ff5c6f6
CKsisodia <cksisodia3@gmail.com>
2024-06-03 17:01:00 +0530

    Task-5

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
+//Module Imports
 const express = require("express");
-const bodyParser = require("body-parser");
-
 const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const userRoutes = require("./routes/user");
-const chatRoutes = require("./routes/chat");
+//My Imports
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const contactUsRoutes = require("./routes/contact");
+const successRoutes = require("./routes/success");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/login", userRoutes);
-app.use("/chat", chatRoutes);
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use("/contact-us", contactUsRoutes);
+app.use("/success", successRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not Found 🧑‍💻</h1>");
+app.use("/", (req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
