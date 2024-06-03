108a417d5514fbcbe9471056debd1a95b7ce29ed
CKsisodia <cksisodia3@gmail.com>
2024-06-03 20:12:55 +0530

    Task-7

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/errror')
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -22,8 +24,6 @@ app.use(shopRoutes);
 app.use("/contact-us", contactUsRoutes);
 app.use("/success", successRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
