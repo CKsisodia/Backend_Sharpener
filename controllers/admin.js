e4007bd16e6ca44a044ff612688bfc1b7578353d
CKsisodia <cksisodia3@gmail.com>
2024-06-07 18:32:02 +0530

    Task-10

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,13 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
     formsCSS: true,
     productCSS: true,
-    activeAddProduct: true
+    activeAddProduct: true,
+    editing: false,
   });
 };
 
@@ -15,17 +16,51 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
-  res.redirect('/');
+  res.redirect("/");
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.id;
+  const uTitle = req.body.title;
+  const uPrice = req.body.price;
+  const uImageurl = req.body.imageUrl;
+  const uDescription = req.body.description;
+  const uProduct  = new Product(prodId, uTitle, uImageurl, uPrice, uDescription)
+   uProduct.save()
+   res.redirect('/admin/products')
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
       prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
+      pageTitle: "Admin Products",
+      path: "/admin/products",
     });
   });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    res.redirect("/");
+  }
+  const prodId = req.params.id;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: editMode,
+      product: product,
+    });
+  });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.id;
 };
