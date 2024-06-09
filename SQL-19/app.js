fc634b1af985955a48c74dfc32ea3132e9b0d318
CKsisodia <cksisodia3@gmail.com>
2024-06-09 17:22:27 +0530

    Task-19

---
+++ SQL-19/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const sequelize = require("./util/database");
+const expenseRoutes = require("./routes/expense");
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(bodyParser.json({ extended: true }));
+
+app.use("/expenses", expenseRoutes);
+
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
